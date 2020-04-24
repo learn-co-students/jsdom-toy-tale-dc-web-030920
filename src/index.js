ce3fc90d2202a0e7976e64694d5fa7ba2d5cf281
rasdinho <ras.dinho7@gmail.com>
2020-04-24 11:05:15 -0400

    message

--- src/index.js
+++ src/index.js
@@ -8,11 +8,34 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyForm.style.display = "block";
+      //put listener here 
+      
     } else {
       toyForm.style.display = "none";
     }
   });
+  const form = document.querySelector("form")
+  form.addEventListener("submit", addNewToy)
 });
+
+function addNewToy(event){
+
+  let toy_obj = {
+    "name": event.target.name.value,
+    "image": event.target.image.value
+  
+  }
+  event.preventDefault()
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {"Content-Type": "application/json",
+    Accept: "application/json"},
+    body: JSON.stringify(toy_obj)
+
+  })
+  .then(response => response.json())
+  .then(newToy => renderToys(newToy))
+}
 // we have to get a GET request to fetch all the toys
 // with the response data make a div 
 {/* <div class="card">
@@ -23,11 +46,17 @@ document.addEventListener("DOMContentLoaded", () => {
   </div> */}
   //add each card to the tog div
 
+  //we should add this event listener when the button is displayed
+
 document.addEventListener('DOMContentLoaded', function(){
   getToys()
   
 })
+function snorLikes(){
 
+
+
+}
 function getToys(){
   fetch("http://localhost:3000/toys")
   .then(response => response.json())
@@ -38,6 +67,8 @@ function renderToys(toy_obj){
   console.log(toy_obj)
   let div = document.createElement("div")
   div.classList.add("card")
+  // so when u render the page
+  div.id = toy_obj.id 
   
   let h2 = document.createElement("h2")
   h2.innerText = toy_obj.name
@@ -53,11 +84,40 @@ function renderToys(toy_obj){
   let btn = document.createElement("button")
   btn.classList.add("like-btn")
   btn.innerText = "Like <3"
+  btn.addEventListener("click", snorLikes)
   
   div.append(h2, img, p, btn)
   
   getToyDiv().appendChild(div)// we have to append the new div to the parent div
   
+}
+function snorLikes(event){
+//patch request
+console.log(event.target)
+let targetDiv = event.target.parentNode
+let id = targetDiv.id 
+
+var currentLikes = targetDiv.querySelector("p").textContent
+
+currentLikes = parseInt(currentLikes.split(" ")[0])
+currentLikes += 1
+
+
+fetch(`http://localhost:3000/toys/${id}`, {
+method: "PATCH",
+headers:{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+},
+
+body: JSON.stringify({
+  "likes": currentLikes
+})
+
+})
+var currentLikesNode = targetDiv.querySelector("p")
+currentLikesNode.innerText = `${currentLikes} Likes`
+event.preventDefault(currentLikes)
 }
 
 function getToyDiv(){
